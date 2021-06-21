ed391e6dfef04aa2a9e41ec78ff924f2ca732da2
BomiAn <qhal4686@gmail.com>
2021-06-22 01:43:39 +0900

    [CodeitLecture] - Python 기초(print, variable, function)

---
+++ first/function.py
@@ -0,0 +1,21 @@
+def hello():
+    print('hello!')
+    print('welcome to Codeit!')
+
+hello()
+hello()
+hello()
+
+# 파라미터
+
+def hello2(name):
+    print('hello!')
+    print(name)
+    print('welcome to Codeit!')
+
+hello2("Chris")
+
+def print_sum(a, b, c):
+    print(a + b + c)
+
+print_sum(7, 3, 10)
---
+++ first/hello.py
@@ -0,0 +1,10 @@
+# 코멘트 == 자바의 주석 // /* */
+print('BomiAn')
+
+print(2 * 5)
+# 자료형 data type 숫자 Integer , Float 문자형 String
+
+print(2 + 5)
+print("2" + "5")
+print(3 < 7)
+print(3 > 7)
