55f418098ffcd71c91c58b7b216c8d7db549e213
BomiAn <qhal4686@gmail.com>
2021-08-10 23:54:00 +0900

    python 기초

--- python/venv/first.py
+++ python/venv/first.py
@@ -46,5 +46,47 @@ print(get_squere(3))
 
 y = get_squere(2)
 print(y)
+#================숫자형
+# 덧셈
+print(4 + 7)
+# 뺄셈
+print(2 - 4)
+#곱셈
+print(5 * 3)
+#나머지
+print(6 % 3)
+#거듭제곱
+print(2 ** 3)
+#나눗셈
+print(4 / 2)
+# 소수 > 정수 하나라도 소수형이면 수수형으로 나옴 나눗셈은 항상 소수형으로 나옴
+# 일반적인 사칙연산 법칙을 따름
+print(2 * (2 + 3))
+
+#floor division (버림 나눗셈)
+print(7 // 2) #3.5 => 3
+print(8.0 // 3) #2.666 => 2.0 둘중 하나가 소수형이면 소수형으로 출력
+#round
+print(round(3.1415926135, 2)) # 그냥 쓰면 소숫점 밑으로는 출력 x
+
+# ==========문자형
+print('hello ' * 3)
+print("'I'm excited to learn Python!")
+print("I\'m \"excited\" to learn Python!")
+print("hello" + " world")
+print("3" + "5")
+
+#=========== 형변환
+
+print(int(3.6))
+print(float(2))
+print(int("2") + int("5"))
+print(float("2.5") + float("1.6"))
+print(str(2) + str(5))
+age = 7
+print("제 나이는 " + str(age) + "살 입니다.")
+
+
+
 
 
